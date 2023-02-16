8645fa0c889db967fe2494d3ad030ee1eba837f9
Redge Real <jrgreal@gmail.com>
2023-02-16 16:33:10 +0800

    Initial Commit

---
+++ SocialProfile/SocialProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProfileApp.swift
+//  SocialProfile
+//
+//  Created by Redge Real on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
